893ec183cf5e91ff3f6867ad0d64f6840de43ce3
JohnLui <wenhan@zhufaner.com>
2023-06-26 21:15:54 +0800

    first blood

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johnlui/enterprise-search-engine
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("My name id enterprise-search-engine!")
+}
